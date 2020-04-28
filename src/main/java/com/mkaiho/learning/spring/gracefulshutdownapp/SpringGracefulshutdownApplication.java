ab1cc74bfe4029ca540560cf99711db472e8ca34
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2020-04-28 15:30:08 +0900

    Add spring project generated by spring initializr

---
+++ src/main/java/com/mkaiho/learning/spring/gracefulshutdownapp/SpringGracefulshutdownApplication.java
@@ -0,0 +1,13 @@
+package com.mkaiho.learning.spring.gracefulshutdownapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGracefulshutdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGracefulshutdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkaiho/learning/spring/gracefulshutdownapp/SpringGracefulshutdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkaiho.learning.spring.gracefulshutdownapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGracefulshutdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
